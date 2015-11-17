0eab05e2b8d1c4ac425ecb3083346058367bcb78
GaryLee <garywlee@gmail.com>
2015-11-17 14:26:07 +0800

    Setup PyPI environment.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!python
+
+from distutils.core import setup
+
+setup(
+  name = 'cmdlet',
+  packages = ['cmdlet'],
+  version = '0.1',
+  description = 'Cmdlet provides pipe-like mechanism to cascade functions and generators.',
+  author = 'Gary Lee',
+  author_email = 'garywlee@gmail.com',
+  url = 'https://github.com/GaryLee/cmdlet', 
+  download_url = 'https://github.com/GaryLee/cmdlet/tarball/0.1',
+  keywords = ['pipe', 'generator', 'iterator'],
+  classifiers = [],
+)

a4324f684f8c9722c74df64e7bef4303ad8cde91
GaryLee <garywlee@gmail.com>
2015-11-20 00:03:00 +0800

    Added stopper pipe.

--- cmdlet/cmdlet.py
+++ cmdlet/cmdlet.py
@@ -296,3 +296,28 @@ class PipeFunction:
                 accum_value = func(accum_value, i, *argv, **kw)
             yield accum_value
         return Pipe(wrapper)
+
+
+    @staticmethod
+    def stopper(func):
+        """Wrap a conditoinal function(stopper function) to Pipe object.
+
+        wrapped function should return boolean value. The cascading pipe will
+        stop the execution if wrapped function return True.
+
+        Stopper is useful if you have unlimited number of input data. 
+
+        :param func: The conditoinal function to be wrapped.
+        :type func: function object
+        :param args: The default arguments to be used for wrapped function.
+        :param kw:  The default keyword arguments to be used for wrapped function.
+        :returns: Pipe object
+        """
+        def wrapper(prev, *argv, **kw):
+            if prev is None:
+                raise TypeError('A stopper must have input.')
+            for i in prev:
+                if func(i, *argv, **kw):
+                    break
+                yield i
+        return Pipe(wrapper)
